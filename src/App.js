cd23f9b23d07ad258a96708b1ae6e832e7ec85b1
ariannargesi <ariannargesi@gmail.com>
2023-01-09 19:01:34 +0330

    refactor: fetching and showing products in one component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Home from "./Home";
-import Products from "./Products";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import ShoppingCart from "./ShoppingCart";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoadingSpinner from "./LoadingSpinner";
+import ProductsList from "./ProductList";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("https://dummyjson.com/products")
-      .then((response) => {
-        setLoading(false);
-        setProducts(response.data.products);
-      })
-      .catch(() => {
-        setErrorMessage("Unable to fetch user list");
-        setLoading(true);
-      });
-  }, []);
-
   return (
     <Router>
       <div>
@@ -33,16 +13,7 @@ function App() {
       </div>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        
-
-        <Route
-          exact
-          path="/products"
-          element={
-            isLoading ? <LoadingSpinner /> : <Products products={products} />
-          }
-        />
-
+        <Route exact path="/products" element={<ProductsList />} />
         <Route exact path="/shoppingcart" element={<ShoppingCart />} />
       </Routes>
     </Router>
